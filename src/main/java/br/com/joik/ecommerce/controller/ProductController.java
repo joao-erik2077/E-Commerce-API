fff22ef9135a09d3c98dfa58c2ca63a2a8f3deab
joao-erik2077 <joao.crisostomo01@etec.sp.gov.br>
2023-04-15 20:27:46 -0300

    post and put request

--- src/main/java/br/com/joik/ecommerce/controller/ProductController.java
+++ src/main/java/br/com/joik/ecommerce/controller/ProductController.java
@@ -5,10 +5,7 @@ import br.com.joik.ecommerce.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -26,11 +23,32 @@ public class ProductController {
     }
 
     @GetMapping("/{id}")
-    private ResponseEntity<Object> getWithId(@PathVariable(value = "id") Long id) {
+    private ResponseEntity<?> getWithId(@PathVariable("id") Long id) {
         Optional<Product> productOptional = productRepository.findById(id);
         if (productOptional.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Not found");
         }
         return ResponseEntity.status(HttpStatus.OK).body(productOptional.get());
+    }
+
+    @PostMapping
+    private ResponseEntity<?> addProduct(@RequestBody Product product) {
+        Product savedProduct = productRepository.save(product);
+        return ResponseEntity.ok(savedProduct);
+    }
+
+    @PutMapping("/{id}")
+    private ResponseEntity<?> changeProduct(@PathVariable("id") Long id, @RequestBody Product updatedProduct) {
+        Optional<Product> optionalProduct = productRepository.findById(id);
+
+        if (optionalProduct.isPresent()) {
+            Product product = optionalProduct.get();
+            product.setName(updatedProduct.getName());
+            product.setPrice(updatedProduct.getPrice());
+            product.setDescription(updatedProduct.getDescription());
+            productRepository.save(product);
+            return ResponseEntity.ok(product);
+        }
+        return ResponseEntity.notFound().build();
     }
 }
